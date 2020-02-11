d2940911d68fa8985df35fd32b75726161eeaa3d
qiyu <qiyu@p1.com>
2020-02-11 11:01:20 +0800

    Initial Commit

---
+++ iOSMultimedia/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSMultimedia
+//
+//  Created by qiyu on 2020/2/11.
+//  Copyright © 2020 com.qiyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iOSMultimedia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSMultimedia
+//
+//  Created by qiyu on 2020/2/11.
+//  Copyright © 2020 com.qiyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
